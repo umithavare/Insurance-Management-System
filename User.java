d5d3ba0b6438bc9653a181bb5eaa5bbe9b4eb834
Umit Havare <umithavare16@gmail.com>
2023-07-11 23:59:19 +0300

    user class updated

--- User.java
+++ User.java
@@ -1,2 +1,93 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class User {
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String profession;
+    private int age;
+    private ArrayList<IAdress> adresses;
+    private Date lastLoginDate;
+
+    public User(String name, String surname, String email,
+                String password, String profession, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.profession = profession;
+        this.age = age;
+        this.adresses = new ArrayList<>();
+
+    }
+
+    public void addAdress(IAdress adress){
+        adresses.add(adress);
+    }
+
+    public void removeAdress(IAdress adress){
+        adresses.remove(adress);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //Getters and setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public Date getLastLoginDate() {
+        return lastLoginDate;
+    }
+
+    public void setLastLoginDate(Date lastLoginDate) {
+        this.lastLoginDate = lastLoginDate;
+    }
+
+
 }
